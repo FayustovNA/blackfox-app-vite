4f6ef4b59f84d4489714f8b4559186384464b146
Nikolay Fayustov <n.a.fayustov@gmail.com>
2023-07-22 16:47:04 +0300

    first

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import styles from './app.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Привет
+      </div>
+    </>
+  )
+}
+
+export default App
